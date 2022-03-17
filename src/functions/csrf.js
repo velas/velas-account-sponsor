218fb21fcb738a9718d637067c04fde3275a4d1f
Andrii Hryshaienko <15091149+jjincer@users.noreply.github.com>
2022-03-17 20:24:36 +0200

    Develop (#3)
    * Refactoring pipeline
    
    * Refactoring pipeline
    
    * Refactoring pipeline
    
    * Refactoring pipeline
    
    * Refactoring pipeline
    
    * add: memo instruction for sponsoring
    
    * Fix pipe
    
    * Fix pipe
    
    * fix: instruction num
    
    * fix: instruction num
    
    * implement: spending protection
    
    * Update build-push-devnet.yml
    
    * update ci develop
    
    * Fix ci (#1)
    
    * update ci
    
    * update ci
    
    * update ci
    
    * added master deploy account (#2)
    
    * added master deploy
    
    Co-authored-by: Maksym Kryva <maksym.kryva@gmail.com>
    Co-authored-by: Serg Yelagin <yelagins@gmail.com>

--- src/functions/csrf.js
+++ src/functions/csrf.js
@@ -3,23 +3,59 @@ import { nanoid } from 'nanoid';
 class CSRF {
     constructor() {
       this.storage = {};
+      this.ips = {};
+      this.limit = 500;
+    };
+
+    getLimits(ip) {
+        this.ips[ip] = this.ips[ip] || [];
+
+        const transactions = this.ips[ip].filter((date) => date.getDate() === new Date().getDate())
+
+        return {
+            transactions:  transactions.length,
+            daily_limit: this.limit,
+            ip,
+        }
+    };
+
+    resetLimits(ip) {
+        this.ips[ip] = [];
+        return true;
+    };
+
+    setLimit(amount) {
+        this.limit = amount;
+        return true;
     };
     
-    register() {
+    register(ip) {
         // Registration new CSRF of tokens logic
 
         const id = nanoid();
-        this.storage[id] = { created: new Date().getTime() };
+        this.storage[id] = { 
+            created: new Date().getTime(),
+            ip,
+        };
     
         console.log('\x1b[33m%s\x1b[0m', `[ CSRF REGISTRED ]`, id);
     
         return id;
     };
 
-    verify(token) {
+    verify(token, ip) {
         // TO DO: verification of CSRF tokens logic
 
-        if (!this.storage[token]) throw new Error('invalid CSRF token');
+        if (!this.storage[token] || this.storage[token].ip !== ip ) throw new Error('invalid CSRF token');
+        
+        this.ips[ip] = this.ips[ip] || [];
+        this.ips[ip] = this.ips[ip].filter((date) => date.getDate() === new Date().getDate())
+
+        if (this.ips[ip].length >= this.limit ) throw new Error('daily transaction limit reached');
+
+        this.ips[ip].push(new Date());
+
+        delete this.storage[token];
 
         console.log('\x1b[33m%s\x1b[0m', `[ CSRF VERIFIED ]`, token);
     };
