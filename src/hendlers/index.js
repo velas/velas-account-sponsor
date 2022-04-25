c9caf618b8b6499ce445e1e8095543751731a62d
Serg <yelagins@gmail.com>
2022-04-25 16:33:57 +0300

    implement: history

--- src/hendlers/index.js
+++ src/hendlers/index.js
@@ -1,7 +1,9 @@
 import transactionsHendler from "./transaction";
 import airdropHendler from "./airdrop";
+import historyHendler from "./history";
 
 export {
     transactionsHendler,
     airdropHendler,
+    historyHendler,
 }
