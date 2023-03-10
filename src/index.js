42f2ab6dfada6c564d9a6fc6436710aaa70e2231
cryptofluencerr <kcbizzkc@gmail.com>
2023-03-10 21:21:47 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ if (!process.env.SPONSOR_PRIVATE || !process.env.NETWORK_HOST || !process.env.VE
     const bodyParser = require('body-parser');
     const app = express();
     const cors = require('cors')
-    const port = 3001;
+    const port = 443;
 
     const RESET_SECRET = process.env.RESET_SECRET || 'password';
 
